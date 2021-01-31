c51f3587ba798993dc05a244b68bc98fad9bcf42
HappyButter <mikolaj.marchewa@gmail.com>
2021-02-01 00:40:22 +0100

    heroku set

--- db/db.js
+++ db/db.js
@@ -3,13 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { Pool } = pg;
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host:  process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: {
+    rejectUnauthorized: false
+    }
 })
 
 export default pool;
--- server.js
+++ server.js
@@ -4,11 +4,11 @@ import cors from 'cors';
 
 import mountRouter from './routes/index.js';
 
-const port = 1000; // tmp port 4 production
+const port = 7312; // tmp port 4 production
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 
 mountRouter(app);
@@ -17,8 +17,8 @@ app.get('/', (req, res) => {
   res.send("Works");
 })
 
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-});
 
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server listening`)
+})
 
